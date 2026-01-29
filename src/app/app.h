158641a36aa96e7cfe986b18521383bb29dcadd0
cuernatra <joel.turkka@gmail.com>
2026-01-29 14:18:52 +0200

    add test ui and change file structure

---
+++ src/app/app.h
@@ -0,0 +1,26 @@
+// app.h
+// main application class
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include "EditorLayer.h"
+
+#ifndef APP_H
+#define APP_H
+
+class App {
+public:
+    App();
+    void run();
+
+private:
+    void processEvents();
+    void update();
+    void render();
+
+    sf::RenderWindow window;
+    sf::Clock deltaClock;
+    EditorLayer editor;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,7 @@
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/Window/Event.hpp>
-
-#include <imgui-SFML.h>
-#include <imgui.h>
+#include "app/App.h"
 
 int main() {
-    // Example view: simple (ImGui + SFML) application
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "ImGui + SFML");
-    window.setFramerateLimit(60);
-    ImGui::SFML::Init(window);
-
-    sf::Clock deltaClock;
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(event);
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-        window.clear();
-        ImGui::SFML::Render(window);
-        window.display();
-    }
-
-    ImGui::SFML::Shutdown();
+    App app;
+    app.run();
     return 0;
 }
