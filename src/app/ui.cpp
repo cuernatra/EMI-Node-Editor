84dc8098c0ea6d502e2146b796c78cbb7c211b4b
Atte Perkio <perkioatte@gmail.com>
2026-02-05 15:07:07 +0200

    Add new ui class

---
+++ src/app/ui.cpp
@@ -0,0 +1,69 @@
+#include "ui.h"
+#include <imgui-SFML.h>
+
+Ui::Ui() 
+{
+}
+
+void Ui::draw()
+{   
+    ImGui::Begin(
+        "EMI EDITOR", 
+        nullptr, 
+        ImGuiWindowFlags_NoMove | 
+        ImGuiWindowFlags_NoResize | 
+        ImGuiWindowFlags_NoCollapse | 
+        ImGuiWindowFlags_NoTitleBar
+    );
+
+    const float totalWidth = ImGui::GetWindowContentRegionMax().x - ImGui::GetWindowContentRegionMin().x;
+    const float minLeft = 50.f;
+    const float minRight = 200.f;
+
+    if(m_leftPanelWidth <= 0.0f)
+    {
+        m_leftPanelWidth = std::clamp(totalWidth * 0.25f, minLeft, totalWidth - minRight - 5.f);
+    }
+
+    ImGui::BeginChild("NODE PALETTE", ImVec2(m_leftPanelWidth, 0), true);
+    m_leftPanel.draw();
+    ImGui::EndChild();
+
+    ImGui::SameLine();
+
+    DrawSplitter(totalWidth, 5.f, 50.f, 200.f);
+
+    ImGui::SameLine();
+
+    ImGui::BeginChild("MAIN EDITOR", ImVec2(0,0), true);
+    m_mainEditor.draw();
+    ImGui::EndChild();
+
+    ImGui::End();
+}
+
+void Ui::DrawSplitter(float totalWidth, float thickness, float minLeft, float minRight)
+{   
+    ImGui::InvisibleButton(
+        "splitter",
+        ImVec2(thickness, ImGui::GetContentRegionAvail().y)
+    );
+
+    // If you hold left mouse button on the splitter
+    if(ImGui::IsItemActive())
+    {   
+        float delta = ImGui::GetIO().MouseDelta.x;
+
+        float newLeft = m_leftPanelWidth + delta;
+        float maxLeft = totalWidth - minRight - thickness;
+
+        // Check that m_leftPanelWidth is between minLeft and maxLeft
+        m_leftPanelWidth = std::clamp(newLeft, minLeft, maxLeft);
+    }
+    
+    // Change cursor to <-> when over the splitter
+    if(ImGui::IsItemHovered() || ImGui::IsItemActive())
+    {
+        ImGui::SetMouseCursor(ImGuiMouseCursor_ResizeEW);
+    }
+}
---
+++ src/app/ui.h
@@ -0,0 +1,20 @@
+#ifndef UI_H
+#define UI_H
+
+#include "mainEditor.h"
+#include "leftPanel.h"
+
+class Ui
+{
+public:
+    Ui();
+    void draw();
+private:
+    MainEditor m_mainEditor;
+    LeftPanel m_leftPanel;
+    float m_leftPanelWidth = -1.f;
+    float m_rightPanelWidth;
+    void DrawSplitter(float totalWidth, float thickness, float minLeft, float minRight);
+};
+
+#endif
