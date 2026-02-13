2f61f481c3dbb682bfb976edfd60b2e68450e270
Atte Perkio <perkioatte@gmail.com>
2026-02-13 11:31:25 +0200

    Refactor code

---
+++ src/app/node.h
@@ -0,0 +1,39 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include "imgui_node_editor.h"
+#include <vector>
+#include <string>
+
+namespace ed = ax::NodeEditor;
+
+struct IdGen 
+{
+    int next = 1;
+    ed::NodeId NewNode() { return ed::NodeId(next++); }
+    ed::PinId  NewPin()  { return ed::PinId(next++); }
+    ed::LinkId NewLink() { return ed::LinkId(next++); }
+};
+
+struct SimpleNode 
+{
+    ed::NodeId id{};
+    ed::PinId  inPin{};
+    ed::PinId  outPin{};
+    std::string title;
+    ImVec2 initialPos{0,0};
+    bool positioned = false;
+    bool alive = true;
+};
+
+struct Link
+{
+    ed::LinkId id;
+    ed::PinId  startPinId; // output
+    ed::PinId  endPinId;   // input
+};
+
+SimpleNode CreateSimpleNode(IdGen& gen, std::string title, ImVec2 pos);
+void DrawSimpleNode(SimpleNode& n);
+
+#endif
