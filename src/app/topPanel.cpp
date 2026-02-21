1415443c23a22a87d2127d93fde94f3ec4afe9fb
Leevi Isotalo <leevi.isotalo@tuni.fi>
2026-02-21 22:22:13 +0200

    Add topPanel buttons

--- src/app/topPanel.cpp
+++ src/app/topPanel.cpp
@@ -6,6 +6,23 @@ TopPanel::TopPanel() : m_height{elementSizes::topBarHeight}
 
 void TopPanel::draw()
 {   
-    ImGui::SetCursorPos(ImVec2{0, 0});
-    ImGui::Dummy(ImVec2{appConstants::windowWidth, m_height});
+    ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 3);
+
+    ImGui::PushStyleColor(ImGuiCol_Button, colors::gray);
+    ImGui::PushStyleColor(ImGuiCol_ButtonHovered, colors::lightGray);
+    ImGui::PushStyleColor(ImGuiCol_ButtonActive, colors::gray);
+
+    if (ImGui::Button("test1", ImVec2(80, m_height - 16)))
+    {
+    }
+
+    ImGui::SameLine(0, 5);
+
+    if (ImGui::Button("test2", ImVec2(80, m_height - 16)))
+    {
+    }
+
+    ImGui::PopStyleColor(3);
+
+    ImGui::PopStyleVar();
 }
