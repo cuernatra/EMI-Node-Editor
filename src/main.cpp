eb040f38a928f9d0abc49bec355d45660c218112
cuernatra <joel.turkka@gmail.com>
2026-02-26 19:11:44 +0200

    fixed EMI implementation + little EMI demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include "app/app.h"
 #include "EMI/EMI.h"
+#include "Parser/Node.h"
+
+static void print_num(double x)
+{
+    std::cout << x << "\n";
+}
+
+EMI_REGISTER(print, print_num)
 
 int main()
 {
-    EMI::SetLogLevel(EMI::LogLevel::Info);
-    auto env = EMI::CreateEnvironment();
+    std::cout << "START\n";
 
+    EMI::SetLogLevel(EMI::LogLevel::Debug);
+
+    Node test;
+    test.data = 123.0;
+    std::cout << VariantToInt(&test) << "\n";
+
+    auto env = EMI::CreateEnvironment();
     std::cout << "ENV OK\n";
 
-    auto script = env.CompileTemporary("1 + 2;");
-    std::cout << "TEST CODE COMPILED\n";
+    auto script = env.CompileTemporary("print(1 + 2);");
+    if (!script.wait()) {
+        std::cout << "SCRIPT FAIL\n";
+    } else {
+        std::cout << "SCRIPT DONE\n";
+    }
 
     App app;
     app.run();
