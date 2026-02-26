cdbeb54c0ad43a483016d4ee340842f32b2c357b
cuernatra <joel.turkka@gmail.com>
2026-02-26 16:31:19 +0200

    add EMI implemented to project (CMake changes)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
+#include <iostream>
 #include "app/app.h"
+#include "EMI/EMI.h"
 
 int main()
 {
+    EMI::SetLogLevel(EMI::LogLevel::Info);
+    auto env = EMI::CreateEnvironment();
+
+    std::cout << "ENV OK\n";
+
+    auto script = env.CompileTemporary("1 + 2;");
+    std::cout << "TEST CODE COMPILED\n";
+
     App app;
     app.run();
-    
+
+    std::cout << "APP EXIT\n";
+    EMI::ReleaseEnvironment(env);
     return 0;
 }
